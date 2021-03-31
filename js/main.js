a5af04654470c188dcc6d25e2c4026710188ea52
Henry Nichols <nicholsh562@gmail.com>
2021-03-31 11:51:48 -0700

    Completed User can create an entry #1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,51 @@
 /* global data */
 /* exported data */
+var imgInput = document.getElementById('imageInput');
+var imgOutput = document.getElementById('imageOutput');
+var form = document.querySelector('form');
+
+var nextEntryId = 0;
+var nextEntryIdStorage = localStorage.getItem('code-journal-next-id');
+if (nextEntryIdStorage == null) {
+  nextEntryId = 0;
+} else {
+  nextEntryId = nextEntryIdStorage;
+}
+
+var dataModel = [];
+var storage = localStorage.getItem('code-journal');
+if (storage === null) {
+  dataModel = [];
+} else {
+  dataModel = JSON.parse(storage);
+}
+
+imgInput.addEventListener('blur', function (event) {
+  var imgURL = imgInput.value;
+  imgOutput.src = imgURL;
+});
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var title = event.currentTarget[0].value;
+  var photoUrl = event.currentTarget[1].value;
+  var notes = event.currentTarget[2].value;
+
+  var formValue = {
+    id: nextEntryId,
+    title: title,
+    url: photoUrl,
+    notes: notes
+  };
+
+  dataModel.unshift(JSON.stringify(formValue));
+  nextEntryId++;
+
+  localStorage.setItem('code-journal', dataModel);
+  localStorage.setItem('code-journal-next-id', nextEntryId);
+
+  form.reset();
+  imgOutput.removeAttribute('src');
+
+  imgOutput.setAttribute('src', './images/placeholder-image-square.jpg');
+});
