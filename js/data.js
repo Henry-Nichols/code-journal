9c9bf728f1b906d5f8a65cb461939d67487b9fb1
Henry Nichols <nicholsh562@gmail.com>
2021-03-31 17:39:04 -0700

    fixed main.js and data.js

--- js/data.js
+++ js/data.js
@@ -1,8 +1,16 @@
 /* exported data */
-
 var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
+
+var storage = localStorage.getItem('code-journal');
+if (storage !== null) {
+  data = JSON.parse(storage);
+}
+
+window.addEventListener('beforeunload', event => {
+  localStorage.setItem('code-journal', JSON.stringify(data));
+});
--- js/main.js
+++ js/main.js
@@ -1,21 +1,8 @@
 /* global data */
 /* exported data */
-
 var imgInput = document.getElementById('imageInput');
 var imgOutput = document.getElementById('imageOutput');
 var form = document.querySelector('form');
-
-var data = {
-  view: 'entry-form',
-  entries: [],
-  editing: null,
-  nextEntryId: 1
-};
-
-var storage = localStorage.getItem('code-journal');
-if (storage !== null) {
-  data = JSON.parse(storage);
-}
 
 imgInput.addEventListener('blur', function (event) {
   var imgURL = imgInput.value;
@@ -24,9 +11,10 @@ imgInput.addEventListener('blur', function (event) {
 
 form.addEventListener('submit', function (event) {
   event.preventDefault();
-  var title = event.currentTarget[0].value;
-  var photoUrl = event.currentTarget[1].value;
-  var notes = event.currentTarget[2].value;
+
+  var title = form.elements[0].value;
+  var photoUrl = form.elements[1].value;
+  var notes = form.elements[2].value;
 
   var formValue = {
     id: data.nextEntryId,
@@ -37,11 +25,6 @@ form.addEventListener('submit', function (event) {
 
   data.entries.unshift(formValue);
   data.nextEntryId++;
-
-  localStorage.setItem('code-journal', JSON.stringify(data));
-
   form.reset();
-  imgOutput.removeAttribute('src');
-
   imgOutput.setAttribute('src', './images/placeholder-image-square.jpg');
 });
