c6f3dec48d79e8efc3c20cebc9c05c53e0fe33e8
Henry Nichols <nicholsh562@gmail.com>
2021-04-01 17:38:44 -0700

    Fixed-Issue-2-View-Their-Entries

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ var form = document.querySelector('form');
 var newEntryBtn = document.getElementById('new-btn-id');
 var entryForm = document.getElementById('entry-form');
 var viewContainer = document.getElementById('view-entries');
+var entryForm = document.getElementById('entry-form');
+var ul = document.getElementById('unlisted');
 
 imgInput.addEventListener('blur', function (event) {
   var imgURL = imgInput.value;
@@ -39,3 +41,40 @@ newEntryBtn.addEventListener('click', function (e) {
   entryForm.classList.remove('hidden');
   viewContainer.classList.add('hidden');
 });
+window.addEventListener('DOMContentLoaded', function (event) {
+  entryForm.classList.add('hidden');
+  for (var i = 0; i < data.entries.length; i++) {
+    var element = entryData(data.entries[i]);
+    ul.appendChild(element);
+  }
+});
+
+function entryData(journalEntry) {
+
+  var row = document.createElement('div');
+  row.classList.add('entryRow');
+
+  var imageDiv = document.createElement('div');
+  imageDiv.classList.add('column-half');
+  row.appendChild(imageDiv);
+
+  var columnImage = document.createElement('img');
+  columnImage.classList.add('inputImage');
+  columnImage.src = journalEntry.url;
+  imageDiv.appendChild(columnImage);
+
+  var columnHalfTwo = document.createElement('div');
+  columnHalfTwo.classList.add('column-half');
+  row.appendChild(columnHalfTwo);
+
+  var headerTitle = document.createElement('h2');
+  headerTitle.classList.add('inputHeader');
+  columnHalfTwo.appendChild(headerTitle);
+  headerTitle.textContent = journalEntry.title;
+
+  var textArea = document.createElement('p');
+  columnHalfTwo.appendChild(textArea);
+  textArea.textContent = journalEntry.notes;
+
+  return row;
+}
